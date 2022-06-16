37c8fab8218b2588144b8986d95a7a0e36e9daad
12345 <12345@DESKTOP-UPKPL2U>
2022-06-16 09:57:16 +0530

    My first commit

---
+++ basicpractice/src/basicpractice/stringtochatarray.java
@@ -0,0 +1,13 @@
+package basicpractice;
+
+public class stringtochatarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1="hello";  
+		char[] ch=s1.toCharArray();  
+		for(int i=0;i<ch.length;i++){  
+		System.out.print(ch[i]);  
+		}  
+		}}  
